feaf7cbeb16f9b4cf30bd3f4089000eaef3d5612
Harshal Vibhandik <harshak.k.vibhandik@gmail.com>
2023-12-31 10:07:35 +0530

    10 Useful Links Additional Info

---
+++ src/main/kotlin/10_Useful_Links_Additional_Info.kt
@@ -0,0 +1,14 @@
+/*
+
+Useful Links Additional Info
+
+*/
+
+/*
+When it comes to job interviews for a Kotlin developer position,
+there are plenty of questions that can be asked.
+
+Here is a link with 50 of those potential questions and their answers.
+
+https://www.ubuntupit.com/frequently-asked-kotlin-interview-questions-and-answers/
+*/
